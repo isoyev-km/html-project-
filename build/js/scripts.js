7a8a488494c801cadd0e6261f9a6c87f0f864863
EvelinaK <evelinalg79@gmail.com>
2020-05-30 10:41:56 +0300

    slider

---
+++ build/js/scripts.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$(window).load(function () {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 210,
+    itemMargin: 5
+  });
+});
